e543db69467695b5fcce01e1e65a727fd98bee71
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-13 15:52:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
-    array.find()
+  if array.include?(cheese_types) 
+    array.find() { |i| i = || i % 7 == 0 }
 end
